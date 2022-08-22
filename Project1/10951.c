3420032f0d4527b5f3c71b8e0d9ca0667917d65c
민구 강 <alsrn7414@kyonggi.ac.kr>
2022-08-22 21:08:45 +0900

    /* EOF(end of file): 파일 끝에 도달했을 때 특별한 값 반환(-1)  최대 몇개의 입력이 들어오는지 모를때 사용*/

---
+++ Project1/10951.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int A, B;
+
+	while (scanf("%d %d", &A, &B) != EOF)
+	{
+		printf("%d\n", A + B);
+	}
+
+}
+/* EOF(end of file): 파일 끝에 도달했을 때 특별한 값 반환(-1)
+ 최대 몇개의 입력이 들어오는지 모를때 사용..*/
