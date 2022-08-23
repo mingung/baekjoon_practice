a23cc0cbb9df1778263ebad96c545f55da1bc059
mingung <alsrn7414@kyonggi.ac.kr>
2022-08-23 20:07:30 +0900

    20220823

---
+++ Project1/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int T, A, B, i;
+	scanf("%d", &T);
+
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		printf("Case #%d: %d\n", i + 1, A + B);
+	}
+}
+// ���̽��� ������ ���� �� �� ���̽� ������ ���� ����ϴ� ���α׷�
