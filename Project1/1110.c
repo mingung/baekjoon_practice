8076c6baccd960c0235c133eab4081052f876bce
mingung <alsrn7414@kyonggi.ac.kr>
2022-08-29 16:25:12 +0900

    1110

--- Project1/1110.c
+++ Project1/1110.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 int main()
 {
-	int N;
+	int N, a, b;
+	int i = 0;
+	int result_clear;
 	scanf("%d", &N);
-
-	if (N >= 10)
+	a = N/10;
+	b = N%10;
+	result_clear = N;
+	while(1)
 	{
+		a = result_clear/10;
+		b = result_clear%10;
+		result_clear = b*10 + (a+b)%10;
+		// 68
+		// 84
+		// 42
+		// 26
+		i++;
+		if (result_clear == N)
+		{
+			printf("%d", i);
+			break;
+
+		}
 
 	}
 }
