10f5927b6aab793aa01c76f5a8b8e133701e37ee
mingung <alsrn7414@kyonggi.ac.kr>
2022-09-11 18:28:29 +0900

    20220911

---
+++ Project1/10172.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char A = '*';
+    char B = ' ';
+    int T, i, j;
+    scanf("%d", &T);
+
+    for (i = 0; i < T; i++)
+    {
+        for (j =0; j < T; j++)
+        {
+            printf("%c\n", A);
+        }
+    }
+
+
+}
