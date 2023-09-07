96ae641152a034457f6c6a11c20e5574262619ac
mingung <alsrn7414@gmail.com>
2023-09-07 23:01:19 +0900

    pc_counter 제작

--- Project1/verilog_practice/pc.v
+++ Project1/verilog_practice/pc.v
@@ -22,6 +22,12 @@ assign s = x ^ y; // xor = 두 수가 같으면 0
 assign c = x & y;
 endmodule // half-adder
 
+module ha_no_carry(s, x, y);
+output s;
+input x, y;
+assign s = x ^ y;
+endmodule
+
 module ha8(data_in, data_out, pc_inc);
 input [7:0] data_in;
 input pc_inc;
@@ -31,21 +37,24 @@ genvar i; //gernerate 블록에서 반복문과 함께 사용
 generate // for 활용 -> 여러 모듈을 반복적 생성
     ha ha0(data_out[0], a[0], pc_inc, data_in[0]);
     for (i = 1; i < 8; i = i + 1) begin:hagen // begin:hagen = 코드 블록을 이름으로 구분 할 수 있게 해줌
-    ha ha1(data_out[i], a[i], a[i - 1], data_in[i]);
+    ha ha1(data_out[i], a[i], a[i - 1], data_in[i]); // 7개의 비트 제작
     end
 endgenerate
+ha_no_carry ha7(data_out[7], a[6], data_in[7]); // 마지막 비트 Cout제거, S만 출력
 endmodule
 
-module pc(clk, clr, pc_inc, load_pc, pc_oen, pc_input, pc_out);
-input clk, clr, pc_inc, load_pc, pc_oen;
+module pc(clk, clr, pc_inc, load_pc, pc_oen, pc_input, pc_out, clk_en);
+input clk, clr, pc_inc, load_pc, pc_oen, clk_en;
 input [7:0] pc_input;
 output [7:0] pc_out;
 wire [7:0] a;
 wire [7:0] b;
 wire [7:0] c;
+wire clk_gate;
 // ha8(datta_in, data_out, pc_inc);
 ha8 u0(.data_in(a), .data_out(b), .pc_inc(pc_inc)); // module에 wire a, b, c 각각 할당
 assign c = (load_pc)?pc_input:b; //MUX
+assign clk_gate = clk & clk_en;
 // reg8(data_out, data_in, inen, oen, clk, clr);
 reg8 u1(a, c, 1'b1, 1'b1, clk, clr);
 assign pc_out = (pc_oen)?a:8'bz; // Tri-state buffer
